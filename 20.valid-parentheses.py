f7ddc4920809f6d20ccf0cb900da70887ee046ea
sagerkudrick <sagerkudrick@gmail.com>
2024-10-01 16:38:29 -0400

    done

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -7,39 +7,58 @@
 # @lc code=start
 class Solution(object):
 
-    def complement(self, s):
-        if s == "(":
-            return ")"
-        elif s == "{":
-            return "}"
-        elif s == "[":
-            return "]"
-
-
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        
+
+        # if s is empty, there's no unclosed brackets
         if len(s) == 0:
             return True
-        elif len(s) == 0:
+        
+        # if s == 1, there's an unopened/unclosed bracket
+        if len(s) == 1:
             return False
 
-        for bracket in s:
-            complement_bracket = self.complement(bracket)
-            s = s[1:]
-            found_comp = False
-            for idx, next_bracket in enumerate(s):
-                if next_bracket == complement_bracket:
-                    found_comp = True
-                    s = s[:idx] + s[idx:]
-                    break
-            if found_comp == False:
-                return False
+        # empty stack + matching bracket dict
+        stack = []
+        matching_brackets = {
+            "[" : "]",
+            "(" : ")",
+            "{" : "}"
+        }
 
+        # for every character in our list
+        # we add the opened bracket to our stack
+        # or, if it's a closing bracket, check if it
+        # has an opening bracket 
+        for char in s:
 
+            # add the opening bracket to stack
+            if char in matching_brackets:
+                stack.append(char)
+
+            # it's a closing bracket, 
+            # check if we have a opening bracket for it
+            else:
+                # we encounter a closing bracket with no opening ones
+                if stack == []:
+                    return False
+                
+                item = stack.pop() # get an opening bracket
+                
+                # if ) != matching_brackets["("] -> ) (False)
+                if char != matching_brackets[item]:
+                    return False
+                
+        # if there's still opening brackets in stack, it wasn't closed
+        # if the stacks empty, all opening/closing brackets are done
+        if len(stack) > 0:
+            return False
+        else:
+            return True
+                    
 
 
 # @lc code=end
